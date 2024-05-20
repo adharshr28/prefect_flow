2b34bc4433c1e96a60c35d282f0f7427fcf37ab1
adharshr28 <adharshr28@gmail.com>
2024-05-20 19:43:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,11 +9,16 @@ def task_one():
 def task_two():
     print("Executing Task Two")
 
+@task
+def task_three():
+    print("Executing Task three")
+
 
 @flow(name="main", log_prints=True)
 def flow():
     task_one()
     task_two()
+    task_three()
 
 if __name__ == "__main__":
     flow()
